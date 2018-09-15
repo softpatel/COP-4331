72ccb36e710e29050e509a6f6a9cd9f0a58b8409
SopheapSok <42819847+SopheapSok@users.noreply.github.com>
2018-09-15 18:25:35 -0400

    Update index.js

--- Sprint-1/Code/index.js
+++ Sprint-1/Code/index.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
+var path = require("path")
 
 app.get("/", function(req, res) {
   res.send("Welcome to the Study App!");
 });
 
-app.listen(port);
+app.get("/menu", function(req,res) {
+    res.send("This is where the menu will be.");
+});
+
+app.get("/timer", function(req,res) {
+    res.sendFile(path.join(__dirname+'/timer.html'));
+})
+
+app.listen(port, function() {
+  console.log("Study app has been started!");
+});
