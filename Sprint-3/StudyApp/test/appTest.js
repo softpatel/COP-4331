a236229885d27c7ce1bf87af27b89a5d6ed90c7a
softpatel <nilp96@outlook.com>
2018-11-11 18:44:45 -0500

    Update appTest.js

--- Sprint-3/StudyApp/test/appTest.js
+++ Sprint-3/StudyApp/test/appTest.js
@@ -17,16 +17,6 @@ describe("App routes", function(){
         }); 
     });
     
-    it("Flashcard edit page is 200", function(done){
-      chai.request(app)
-        .get("flashcards/5be0dbc233236b228d26c039/edit")
-        .end(function(err, res){
-          if(err) console.log(err);
-          res.should.have.status(200);
-          done();
-        }); 
-    });
-    
     it("login/homepage status is 200", function(done){
       chai.request(app)
         .get("/")
@@ -119,39 +109,5 @@ describe("App routes", function(){
           done();
         });
     });
-  });
-  
-  describe("PUT routes", function() {
-    it("Update flashcard page status is 200", function(done){
-      chai.request(app)
-        .post("/flashcards/example")
-        .end(function(err, res){
-          if(err) console.log(err);
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-  
-  describe("DELETE routes", function() {
-    it("Delete flashcard page status is 200", function(done){
-      chai.request(app)
-        .post("/deleteCard/5be0dbc233236b228d26c039")
-        .end(function(err, res){
-          if(err) console.log(err);
-          res.should.have.status(200);
-          done();
-        });
-    });
-    
-    it("Delete note page status is 200", function(done){
-      chai.request(app)
-        .post("/5bda4ea6033bf606ce57f63b")
-        .end(function(err, res){
-          if(err) console.log(err);
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
+  }); 
 });
